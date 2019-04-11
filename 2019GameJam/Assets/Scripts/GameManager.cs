d0c5cbd3910273597ca9d7c8ff4ee3a0c5a8d7b3
Thebarron1985 <tylerratty@gmail.com>
2019-04-11 16:01:24 +1000

    Added scripts and test scenes into main file. Added Player Car Prefab.

---
+++ 2019GameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public float timer;
+    private bool isActive;
+    public Text[] text_UI;
+  
+
+	// Use this for initialization
+	void Start () {
+        isActive = false;
+        timer = 0;
+
+       for(int i = 0; i < text_UI.Length; i++)
+        {
+            text_UI[i].enabled = false;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(isActive == false)
+        {
+            CountDown();
+        }
+	}
+
+    public void CountDown()
+    {
+        if(timer >= 1)
+        {
+            text_UI[0].enabled = true;
+
+            if (timer >= 2)
+            {
+                text_UI[0].enabled = false;
+                text_UI[1].enabled = true;
+
+                if (timer >= 3)
+                {
+                    text_UI[1].enabled = false;
+                    text_UI[2].enabled = true;
+
+                    if (timer >= 4)
+                    {
+                        text_UI[2].enabled = false;
+                        text_UI[3].enabled = true;
+
+                        isActive = true;
+                    }
+                }
+            }
+        }
+    }
+}
