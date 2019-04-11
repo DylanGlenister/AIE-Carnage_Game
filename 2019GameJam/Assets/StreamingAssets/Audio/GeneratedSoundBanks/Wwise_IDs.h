e224c79a7625da73ff72052f801a1283c3c3d27c
Sam <sammydeedge@hotmail.com>
2019-04-11 16:31:00 +1000

    Started Wwise Implementation
    Have started Implementing Wwise.
    
    Done:
    - Added all the framework for the Wwise Project
    - Integrated with Unity Project... mostly
    
    TODO:
    - Fix bugs in Unity-Wwise Integration
    - Add new music
    - Add more SFX

---
+++ 2019GameJam/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,134 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID SCNAWAKE = 2368342272U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace SCENE
+        {
+            static const AkUniqueID GROUP = 1926883983U;
+
+            namespace STATE
+            {
+                static const AkUniqueID SCNFORESTTRACK = 2241817681U;
+                static const AkUniqueID SCNMAINMENU = 3797992041U;
+            } // namespace STATE
+        } // namespace SCENE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace COLLIDER
+        {
+            static const AkUniqueID GROUP = 3721930865U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CHECKPOINT = 612075679U;
+                static const AkUniqueID FINISH_CHECKPOINT = 1487719781U;
+                static const AkUniqueID OTHERCAR = 3471752377U;
+                static const AkUniqueID WALL = 2108779961U;
+            } // namespace SWITCH
+        } // namespace COLLIDER
+
+        namespace P1_CARCHOICE
+        {
+            static const AkUniqueID GROUP = 2352788226U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID P1_CAR1 = 1151487140U;
+                static const AkUniqueID P1_CAR2 = 1151487143U;
+                static const AkUniqueID P1_CAR3 = 1151487142U;
+            } // namespace SWITCH
+        } // namespace P1_CARCHOICE
+
+        namespace P1_DRIVINGON
+        {
+            static const AkUniqueID GROUP = 720808783U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID P1_ONGRASS = 2354807538U;
+                static const AkUniqueID P1_ONROAD = 2273424654U;
+            } // namespace SWITCH
+        } // namespace P1_DRIVINGON
+
+        namespace P2_CARCHOICE
+        {
+            static const AkUniqueID GROUP = 920705041U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID P2_CAR1 = 2544039433U;
+                static const AkUniqueID P2_CAR2 = 2544039434U;
+                static const AkUniqueID P2_CAR3 = 2544039435U;
+            } // namespace SWITCH
+        } // namespace P2_CARCHOICE
+
+        namespace P2_DRIVINGON
+        {
+            static const AkUniqueID GROUP = 3328732056U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID P2_ONGRASS = 3704478681U;
+                static const AkUniqueID P2_ONROAD = 3026002255U;
+            } // namespace SWITCH
+        } // namespace P2_DRIVINGON
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID P1_SPEED = 690232500U;
+        static const AkUniqueID P2_SPEED = 1648238627U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GENERAL = 133642231U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
