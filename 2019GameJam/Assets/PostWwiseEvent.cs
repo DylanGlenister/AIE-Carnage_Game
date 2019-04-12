dc0a1f57d44f3c517a020c1945758341b29f9611
Sam <sammydeedge@hotmail.com>
2019-04-12 13:16:43 +1000

    Finalised Wwise Integration
    Finally the bloody audio's working

---
+++ 2019GameJam/Assets/PostWwiseEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostWwiseEvent : MonoBehaviour
+{
+
+    public AK.Wwise.Event myEvent;
+
+    public void postEvent()
+    {
+        myEvent.Post(gameObject);
+    }
+};
